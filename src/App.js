86817683ca24ed8ebe8a8b57e6fa4db862283e84
subhabrata <malliksubhabrata@gmail>
2023-12-05 12:36:09 +0530

    frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [messages, setMessages] = useState([]);
+  const [Chat, setChat] = useState('');
+  const [inputValue, setInputValue] = useState('');
+
+  
+  // const data=(data)=>{
+  //   setChat(data);
+  // }
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleSendMessage = () => {
+    if (inputValue.trim() !== '') {
+      const newChat = { text: inputValue, user: 'user' };
+      const newChatMessage = { text: inputValue, user: 'chat' };
+      
+      setMessages([...messages, newChat, newChatMessage]);
+      setInputValue('');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="chat-container">
+        <div className="chat-messages">
+          {messages.map((message, index) => (
+            <div key={index} className={`message ${message.user}`}>
+              {message.text}
+            </div>
+          ))}
+        </div>
+        <div className="chat-input">
+          <input
+            type="text"
+            placeholder="Type Your question ..."
+            value={inputValue}
+            onChange={handleInputChange}
+          />
+          <button onClick={handleSendMessage}>Send</button>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
